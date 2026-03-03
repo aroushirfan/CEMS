c1c4e3e92d68de0bb840168225d9ce2aca918ba6
Puntawat Subhamani <puntawatsub@gmail.com>
2026-03-03 02:52:07 +0200

    Update AuthServiceTest.java

--- frontend/src/test/java/com/cems/frontend/services/AuthServiceTest.java
+++ frontend/src/test/java/com/cems/frontend/services/AuthServiceTest.java
@@ -2,18 +2,36 @@ package com.cems.frontend.services;
 
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthServiceTest {
 
     private final int PORT = Integer.parseInt(System.getenv("PORT"));
+    private MockWebServer server;
+
+    @BeforeEach
+    void beforeEach() {
+        server = new MockWebServer();
+    }
+
+    @AfterEach
+    void afterEach() {
+        if (server != null) {
+            try {
+                server.shutdown();
+            } catch (IOException ignored) {}
+        }
+    }
 
     @Test
     void login() throws Exception {
         AuthService authService = AuthService.getInstance();
-        MockWebServer server = new MockWebServer();
 
         server.enqueue(new MockResponse()
                 .setResponseCode(200)
@@ -23,14 +41,11 @@ class AuthServiceTest {
 
         String token = authService.login("email", "password");
         assertEquals("fake-token", token);
-
-        server.shutdown();
     }
 
     @Test
     void signUp() throws Exception {
         AuthService authService = AuthService.getInstance();
-        MockWebServer server = new MockWebServer();
         server.enqueue(new MockResponse()
                 .setResponseCode(201));
         server.enqueue(new MockResponse()
@@ -40,6 +55,5 @@ class AuthServiceTest {
         server.start(PORT);
         assertDoesNotThrow(() -> authService.signUp("asdf", "asdf", "asdf", "asdf", "asdf"));
         assertThrowsExactly(Exception.class, () -> authService.signUp("asdf", "asdf", "asdf", "asdf", "asdf"), "user already exists");
-        server.shutdown();
     }
 }
