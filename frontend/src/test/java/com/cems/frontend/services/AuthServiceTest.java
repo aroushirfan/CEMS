a9f841dabe6cbc0a7db2c7026ac49695b9d612d6
Puntawat Subhamani <puntawatsub@gmail.com>
2026-03-03 03:12:32 +0200

    fix port issue

--- frontend/src/test/java/com/cems/frontend/services/AuthServiceTest.java
+++ frontend/src/test/java/com/cems/frontend/services/AuthServiceTest.java
@@ -7,37 +7,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuthServiceTest {
-
-    private final int PORT = Integer.parseInt(System.getenv("PORT"));
     private MockWebServer server;
+    private AuthService authService;
 
     @BeforeEach
-    void beforeEach() {
+    void beforeEach() throws IOException {
         server = new MockWebServer();
+        server.start();
+        int port = server.getPort();
+        authService = AuthService.getInstance();
+        authService.setPort(String.valueOf(port));
     }
 
     @AfterEach
-    void afterEach() {
-        if (server != null) {
-            try {
-                server.shutdown();
-            } catch (IOException ignored) {}
-        }
+    void afterEach() throws IOException {
+        server.shutdown();
     }
 
     @Test
     void login() throws Exception {
-        AuthService authService = AuthService.getInstance();
-
         server.enqueue(new MockResponse()
                 .setResponseCode(200)
                 .setBody("{\"token\":\"fake-token\"}"));
-
-        server.start(PORT);
 
         String token = authService.login("email", "password");
         assertEquals("fake-token", token);
@@ -45,14 +41,11 @@ class AuthServiceTest {
 
     @Test
     void signUp() throws Exception {
-        AuthService authService = AuthService.getInstance();
         server.enqueue(new MockResponse()
                 .setResponseCode(201));
         server.enqueue(new MockResponse()
                 .setResponseCode(400)
                 .setBody("{\"error\":\"user already exists\"}"));
-
-        server.start(PORT);
         assertDoesNotThrow(() -> authService.signUp("asdf", "asdf", "asdf", "asdf", "asdf"));
         assertThrowsExactly(Exception.class, () -> authService.signUp("asdf", "asdf", "asdf", "asdf", "asdf"), "user already exists");
     }
