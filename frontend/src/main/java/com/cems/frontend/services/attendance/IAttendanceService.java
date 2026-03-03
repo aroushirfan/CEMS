f7364a8b601ad4b6ce41ad30e6125a0c2069ce21
williams ayo <williamsayo44@gmail.com>
2026-03-03 07:00:58 +0200

    update frontend attendance page design

--- frontend/src/main/java/com/cems/frontend/services/attendance/IAttendanceService.java
+++
@@ -1,9 +0,0 @@
-package com.cems.frontend.services.attendance;
-
-import com.cems.frontend.models.Attendance;
-
-import java.util.List;
-
-public interface IAttendanceService {
-    List<Attendance> getEventAttendance(String eventId) throws Exception;
-}
