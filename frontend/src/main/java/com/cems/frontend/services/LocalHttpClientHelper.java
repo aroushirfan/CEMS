d060e3f6280a5e6d61b57f8accc0f618f1e58bc0
williams ayo <williamsayo44@gmail.com>
2026-03-03 07:02:15 +0200

    make httphelper more customizable

--- frontend/src/main/java/com/cems/frontend/services/LocalHttpClientHelper.java
+++ frontend/src/main/java/com/cems/frontend/services/LocalHttpClientHelper.java
@@ -7,26 +7,33 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.net.URI;
 import java.net.http.HttpRequest;
+import java.net.http.HttpClient;
 
-public class LocalHttpClient {
-    private static final String BASE_URL = "http://localhost:8080/";
-    public static java.net.http.HttpClient  getClient() {
-        return java.net.http.HttpClient.newHttpClient();
+public class LocalHttpClientHelper {
+    private static final String BASE_URL = "http://localhost";
+    private static String PORT = "8080"; // default port
+    private static final HttpClient httpClient = HttpClient.newHttpClient();;
+
+    public static HttpClient getClient() {
+       return httpClient;
     }
 
-    public static ObjectMapper  getMapper() {
+    public static ObjectMapper getMapper() {
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
                 .setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 
+    public static void setPort(String port) {
+        PORT = port;
+    }
+
     public static HttpRequest buildGetRequest (String uri) {
         return HttpRequest.newBuilder()
-                .uri(URI.create(BASE_URL + uri))
+                .uri(URI.create(String.format("%s:%s/%s", BASE_URL,PORT, uri)))
                 .header("Accept", "application/json")
                 .GET()
                 .build();
     }
-
 }
